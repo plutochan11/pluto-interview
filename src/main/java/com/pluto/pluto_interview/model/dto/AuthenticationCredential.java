82cfc5ca94da016b811deb6e505cc992ae76347f
Pluto <109087502+plutochan11@users.noreply.github.com>
2026-01-14 15:53:03 +0800

    Rename and add password length validation

--- src/main/java/com/pluto/pluto_interview/model/dto/AuthenticationCredential.java
+++
@@ -1,7 +0,0 @@
-package com.pluto.pluto_interview.model.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record AuthenticationCredential(@Email @NotBlank String email, @NotBlank String password) {
-}
---
+++ src/main/java/com/pluto/pluto_interview/model/dto/AuthenticationRequest.java
@@ -0,0 +1,14 @@
+package com.pluto.pluto_interview.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record AuthenticationRequest(
+	  @Email
+	  @NotBlank
+	  String email,
+	  @NotBlank
+	  @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+	  String password) {
+}
